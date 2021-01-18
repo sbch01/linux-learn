d4bf81a928144bf2e3376d4540d5239ce447a2fb
sbch01 <banchev2001@abv.bg>
2021-01-18 17:31:26 +0200

    New lessons and bashrc.bak file

---
+++ test/prg.c
@@ -0,0 +1,16 @@
+#include <iostrem.h>
+
+/*Here add some comment about the code*/
+
+main (){
+
+	delay (150); 
+	printf ("Helo world");
+	//Let's add some more code here 
+		while(1){
+		// Endless loop
+		
+		}//end main loop
+
+	return 0;
+}//end main
